52853a9406158bf08e7c02d9e83a57a842efc722
Carl Parrish <carl.parrish@gmail.com>
2022-02-06 21:45:06 -0700

    Clean up code a bit by putting all the inializations together.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,34 +1,37 @@
 package com.company;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
 	// write your code here
-        var scanner = new Scanner(System.in);
-        System.out.print("Enter a numeric value: ");
-        double num1 = 0;
-        try {
-            num1 = scanner.nextDouble();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        System.out.print("Enter a numeric value ");
-        double num2 = 0;
-        try {
-            num2 = scanner.nextDouble();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        System.out.print("Choose an operator (+ - * / % ) ");
+
+        //declare & initialize vars
+        double num1, num2, answer;
+        num1 = num2 = answer = 0;
+
         char operator  = '@'; //place holder
+
+        var scanner = new Scanner(System.in);
+
+
+        try {
+            System.out.print("Enter a numeric value: ");
+            num1 = scanner.nextDouble();
+            scanner.nextLine();
+        } catch (InputMismatchException e) { e.printStackTrace(); }
+
+        try {
+            System.out.print("Enter a numeric value ");
+            num2 = scanner.nextDouble();
+            scanner.nextLine();
+        } catch (InputMismatchException e) { e.printStackTrace(); }
+        System.out.print("Choose an operator (+ - * / % ) ");
+
         try {
             operator = scanner.next().charAt(0);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        double answer = 0;
+        } catch (Exception e) { e.printStackTrace(); }
 
         switch (operator) {
             case '+' -> answer = num1 + num2;
@@ -38,7 +41,6 @@ public class Main {
             case '%' -> answer = num1 % num2;
             default -> System.out.println("Invalid operator entered! ");
         }
-
         System.out.println("The answer is " + answer);
     }
 }
