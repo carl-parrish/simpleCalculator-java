0dc690102dce80205218a9146bc46a019bbf6d37
Carl Parrish <carl.parrish@gmail.com>
2022-02-06 20:58:45 -0700

    Add to repo

---
+++ src/com/company/Main.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        var scanner = new Scanner(System.in);
+        System.out.print("Enter a numeric value: ");
+        double num1 = 0;
+        try {
+            num1 = scanner.nextDouble();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.print("Enter a numeric value ");
+        double num2 = 0;
+        try {
+            num2 = scanner.nextDouble();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.print("Choose an operator (+ - * / % ) ");
+        char operator  = '@'; //place holder
+        try {
+            operator = scanner.next().charAt(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        double answer = 0;
+
+        switch (operator){
+            case '+':
+                answer =  num1 + num2;
+                break;
+            case '-':
+                answer = num1 - num2;
+                break;
+            case '*':
+                answer = num1 * num2;
+                break;
+            case '/':
+                answer = num1 / num2;
+                break;
+            case '%':
+                answer = num1 % num2;
+                break;
+            default:
+                System.out.println("Invalid operator entered! ");
+        }
+
+        System.out.println("The answer is " + answer);
+    }
+}
